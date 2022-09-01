c4ff2dd84b7fbf6e5c4b81a3e054445fce3b5cc4
wejeriwej <108554440+wejeriwej@users.noreply.github.com>
2022-09-01 02:27:51 +0100

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,19 @@
+const startingMinutes = 10;
+  let time = startingMinutes * 60;
+
+  const countdownEl = document.getElementById('countdown');
+  setInterval(updateCountdown, 1000);
+  function updateCountdown(){
+      const minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      countdownEl.innerHTML = "Timer: " + `${minutes}:${seconds}`; 
+      time--;
+      if (time<0){
+        
+        countdownEl.innerHTML = "!!!!!!!!!!!!"+  "<br />"+ "END" +  "<br />"+  "OF" +  "<br />"+ "TIMER" +  "<br />"+ "!!!!!!!!!!!!";
+
+      }
+  }
